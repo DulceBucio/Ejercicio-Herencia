877f3bc8efa8261efcd7a0af52ecf65f450da976
a01425162@tec.mx <a01425162@tec.mx>
2023-05-23 15:08:38 -0600

    Possible Triangulo class implementation

--- ejercicioHerencia/Triangulo.cpp
+++ ejercicioHerencia/Triangulo.cpp
@@ -6,18 +6,38 @@
 //
 
 #include <iostream>
+#include <cmath>
 #include "Figura.h"
 #include "Triangulo.h"
 using namespace std;
 
-double Triangulo::calculaArea() const {
-    //
-}
 
 double Triangulo::calculaPerimetro() const {
-    //
+    return lado1 + lado2 + lado3;
+}
+
+double Triangulo::calculaArea() const {
+    double s = calculaPerimetro() / 2;  // Semiperímetro
+    return sqrt(s * (s - lado1) * (s - lado2) * (s - lado3));  // Fórmula de Herón
 }
 
 void Triangulo::imprime() const {
-    //
+    Figura::imprime();
+    cout << "Triángulo" << "\n";
+    cout << "Lados: " << lado1 << ", " << lado2 << ", " << lado3 << "\n";
+    cout << "Perímetro: " << calculaPerimetro() << "\n";
+    cout << "Área: " << calculaArea() << endl;
 }
+
+
+//double Triangulo::calculaArea() const {
+
+//}
+
+//double Triangulo::calculaPerimetro() const {
+    //
+//}
+
+//void Triangulo::imprime() const {
+    //
+//}
--- ejercicioHerencia/Triangulo.h
+++ ejercicioHerencia/Triangulo.h
@@ -11,11 +11,14 @@ using namespace std;
 #ifndef EJERCICIOHERENCIA_TRIANGULO_H
 #define EJERCICIOHERENCIA_TRIANGULO_H
 
-class Triangulo:public Figura{
+class Triangulo: public Figura{
+    double lado1, lado2, lado3;
 public:
     Triangulo() = default;
     Triangulo(const Triangulo &triangulo) {};
-    Triangulo(const string& nombre, const list<Punto>& puntos) : Figura(3, nombre, puntos) {};
+    //Triangulo(const string& nombre, const list<Punto>& puntos) : Figura(3, nombre, puntos) {};
+    Triangulo(const string& nombre, const list<Punto>& puntos, double lado1, double lado2, double lado3)
+            : Figura(3, nombre, puntos), lado1(lado1), lado2(lado2), lado3(lado3) {}
     virtual ~Triangulo();
     double calculaArea() const override;
     double calculaPerimetro() const override;
